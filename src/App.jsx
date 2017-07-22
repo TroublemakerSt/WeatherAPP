e96260b3eeba0b6936a95f173468f9382cf72fed
TroublemakerSt <bugandcode@gmail.com>
2017-07-22 17:59:49 +0300

    Detecting current city

--- src/App.jsx
+++ src/App.jsx
@@ -17,15 +17,39 @@ class WeatherApp extends Component {
 	}
 
 	fetchIp = () => {
-		fetch('//freegeoip.net/json/')
+		fetch('http://freegeoip.net/json/')
 			.then(response => response.json())
-			.then(({ city }) => console.log(city))
+			.then(({ city }) => this.fetchWeatherData(city))
+			.catch(error => console.log(error));
+	}
+
+	fetchWeatherData = city => {
+		const baseUrl = `http://api.openweathermap.org`;
+		const path = `/data/2.5/weather`;
+		const apiKey = `b3ac746512dc932694a267eb3e447103`;
+		const query = `units=metric&lang=ru&appid=${apiKey}`
+
+		fetch(`${baseUrl}${path}?q=${city}&${query}`)
+			.then(response => response.json())
+			.then(data => {
+				const date = new Date();
+				const time = date.getHours();
+
+				this.setState({
+					time,
+					city,
+					temperature: Math.round(data.main.temp),
+					weatherCode: data.weather[0].id,
+					fetching: false,
+				});
+			})
 			.catch(error => console.log(error));
 	}
 
   render() {
+		const { city } = this.state;
     return (
-      <h1>Hello!</h1>
+      <h1>Hello! Now we in the {city} city! </h1>
     );
   }
 }
