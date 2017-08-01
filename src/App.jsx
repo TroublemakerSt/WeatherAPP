257e6f871ec8e4ca8c1e94e08fdf222711d123a4
TroublemakerSt <bugandcode@gmail.com>
2017-08-01 16:31:04 +0300

    docs

--- src/App.jsx
+++ src/App.jsx
@@ -20,14 +20,14 @@ class WeatherApp extends Component {
 	}
 
 	fetchIp = () => {
-		fetch('http://freegeoip.net/json/')
+		fetch('//freegeoip.net/json/')
 			.then(response => response.json())
 			.then(({ city }) => this.fetchWeatherData(city))
 			.catch(error => console.log(error));
 	}
 
 	fetchWeatherData = city => {
-		const baseUrl = `http://api.openweathermap.org`;
+		const baseUrl = `//api.openweathermap.org`;
 		const path = `/data/2.5/weather`;
 		const apiKey = `b3ac746512dc932694a267eb3e447103`;
 		const query = `units=metric&lang=ru&appid=${apiKey}`
